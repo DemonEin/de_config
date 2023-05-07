8aba1f4eda75a684165ec480f63876708b8fd064
Daniel Warner <danielwarner00@gmail.com>
2023-05-07 14:00:33 -0700

    added autowrite when using rust_analyzer because it only works when written to disk

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -45,7 +45,21 @@ require('lspconfig')['tsserver'].setup{
     flags = lsp_flags,
 }
 require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
+    on_attach = function(client, bufnr)
+
+        vim.api.nvim_create_autocmd({'TextChanged', 'InsertLeave'}, {
+            callback = function()
+                if #vim.lsp.get_active_clients({ name = 'rust_analyzer' }) ~= 0 then
+                    vim.cmd('write')
+                end
+            end,
+            buffer = bufnr,
+            nested = true,
+        })
+
+        on_attach(client, bufnr)
+
+    end,
     flags = lsp_flags,
     -- Server-specific settings...
     settings = {
