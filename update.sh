b71d0c5bacfc489b367ca0a9ac0610f8b644173a
Daniel Warner <danielwarner00@gmail.com>
2023-06-30 16:44:05 -0700

    added system update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if command -v pacman; then
+    sudo pacman -Syu
+    # TODO remove results of pacman -Qte
+fi
+
+if command -v apt; then
+    sudo apt update
+    sudo apt upgrade
+    sudo apt autoremove
+fi
+
+if command -v nvim; then
+     nvim -c 'au User PackerComplete qa' -c 'PackerSync'
+fi
+
+if command -v rustup; then
+    rustup update
+fi
+
