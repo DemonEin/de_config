26716f2fa2ac6061b4c3a1c286d264d3a7804f16
DemonEin <danielwarner00@gmail.com>
2023-11-13 09:07:02 -0800

    added highlights file

---
+++ nvim/queries/brightscript/highlights.scm
@@ -0,0 +1,76 @@
+[
+  "if"
+  "else"
+  "then"
+  "sub"
+  "function"
+  "end"
+  "stop"
+  "goto"
+  "return"
+  "for"
+  "to"
+  "each"
+  "in"
+  "while"
+  "exit"
+  "continue"
+  "try"
+  "catch"
+  "throw"
+] @keyword
+
+[
+  "("
+  ")"
+  "{"
+  "}"
+  ","
+  ";"
+  ":"
+  "as"
+] @punctuation
+
+[
+  "."
+  "["
+  "]"
+  "?"
+  "?."
+  "?@"
+  "^"
+  "*"
+  "/"
+  "\\"
+  "mod"
+  "+"
+  "-"
+  "<<"
+  ">>"
+  "<"
+  ">"
+  "="
+  "<>"
+  "<="
+  ">="
+  "and"
+  "or"
+] @operator
+
+[
+  "print"
+] @function
+
+(type) @type
+
+(string_literal) @string
+
+(number_literal) @number
+
+(function_declaration
+    . (identifier) @function
+)
+
+(function_call_expression
+    . (identifier) @function
+)
