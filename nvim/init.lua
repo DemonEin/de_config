60273709deeeab883f4a317b32169deec448f79c
Daniel Warner <danielwarner00@gmail.com>
2023-11-13 08:45:44 -0800

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,68 @@ require("plugins")
 
 vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
-require("catppuccin").setup()
+require("catppuccin").setup({
+    custom_highlights = function(colors)
+        return {
+            Type = { fg = colors.blue },
+            Function = { fg = colors.yellow },
+            Character = { fg = colors.teal },
+            ['@parameter'] = { fg = colors.text },
+        }
+    end,
+
+    styles = {
+        conditionals = {},
+    },
+})
 
 require'lspconfig'.rust_analyzer.setup{}
 require('lsp-config')
 
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.brightscript = {
+  install_info = {
+    url = "~/tree-sitter-brightscript", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+}
+
+vim.filetype.add({
+    extension = {
+        brs = 'brightscript'
+    }
+})
+
 vim.cmd [[colorscheme catppuccin]]
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+link_highlights = {
+    ['@function.builtin'] = '@function',
+    ['@variable.builtin'] = '@variable',
+    ['@type.builtin'] = '@type',
+    ['StorageClass'] = 'Type',
+    ['Structure'] = 'Type',
+}
+
+for source, target in pairs(link_highlights) do
+    vim.api.nvim_set_hl(0, source, { link = target } )
+end
 
 vim.opt.autowrite = true
 vim.opt.autowriteall = true
