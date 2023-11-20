073bf247eb702b7ddce1f8d8a0ab91ebb76d2439
Daniel Warner <danielwarner00@gmail.com>
2023-11-20 13:57:21 -0800

    added treesitter context and context queries for brightscript

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,4 +23,6 @@ return require('packer').startup(function(use)
   -- use 'entrez/roku.vim'
 
   use 'nvim-treesitter/nvim-treesitter'
+
+  use 'nvim-treesitter/nvim-treesitter-context'
 end)
---
+++ nvim/queries/brightscript/context.scm
@@ -0,0 +1,6 @@
+(function_definition) @context
+(if_block) @context
+(else_block) @context
+(else_if_block) @context
+(for_block) @context
+(while_block) @context
