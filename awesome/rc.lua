13146d2afc2db7414c1e3da226a3eb9402d7f18e
Daniel Warner <danielwarner00@gmail.com>
2025-06-12 20:18:17 -0700

    awesome: comment layouts I don't want

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -72,7 +72,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     -- awful.layout.suit.floating,
-    awful.layout.suit.tile,
+    -- awful.layout.suit.tile,
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
@@ -81,7 +81,7 @@ awful.layout.layouts = {
     -- awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.max.fullscreen,
     -- awful.layout.suit.magnifier,
     -- awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
