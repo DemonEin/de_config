9d9557b6840640bfaf176e80a748f275c9439c28
Daniel Warner <danielwarner00@gmail.com>
2025-11-29 15:10:59 -0800

    minor refactor to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,14 +7,18 @@ sudo -v
 while true; do sleep 60; sudo -v; done &
 trap "kill $!" SIGINT SIGTERM EXIT
 
-PACCACHE_COMMAND='sudo paccache -rk2 --min-atime "4 weeks ago" && sudo paccache -ruk0 --min-atime "4 weeks ago"'
+clear_pacman_cache() {
+    sudo paccache -rk2 --min-atime "4 weeks ago" \
+        && sudo paccache -ruk0 --min-atime "4 weeks ago"
+}
+
 if command -v paru; then
     paru -Syu --combinedupgrade --removemake
-    eval "$PACCACHE_COMMAND"
+    clear_pacman_cache
 elif command -v pacman; then
     sudo pacman -Syu --noconfirm
     # TODO remove results of pacman -Qte
-    eval "$PACCACHE_COMMAND"
+    clear_pacman_cache
 fi
 
 if command -v apt; then
