1ff1adb7079f027da3e5c27e99073b0ef137cb3c
Daniel Warner <danielwarner00@gmail.com>
2026-01-26 20:18:45 -0800

    fix locale issue
    use correct name for locale
    
    also use localectl to set the locale

--- scripts/install-locale.sh
+++ scripts/install-locale.sh
@@ -4,10 +4,10 @@ set -e
 
 SCRIPT_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-LINK=/usr/share/i18n/locales/de-locale
-[ -f "$LINK" ] || sudo ln -T "$SCRIPT_DIRECTORY"/../de-locale "$LINK"
+LINK=/usr/share/i18n/locales/en_US@de-custom
+[ -f "$LINK" ] || sudo ln -T "$SCRIPT_DIRECTORY"/../en_US@de-custom "$LINK"
 
-rg -q '^\s*de-locale\s+UTF-8\s*$' /etc/locale.gen || echo 'de-locale UTF-8' | sudo tee --append /etc/locale.gen > /dev/null
+rg -q '^\s*en_US@de-custom\s+UTF-8\s*$' /etc/locale.gen || echo 'en_US@de-custom UTF-8' | sudo tee --append /etc/locale.gen > /dev/null
 sudo locale-gen
 
-echo LANG=de-locale.UTF-8 | sudo tee /etc/locale.conf > /dev/null
+sudo localectl set-locale en_US@de-custom
